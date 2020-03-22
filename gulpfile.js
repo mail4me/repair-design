69bf3654d83f64df60dc5476f97abba855e6da9a
Юлия Линовицкая <mail4me2401@yandex.ru>
2020-03-22 19:50:31 +0300

    feat: add normalize, grid, variable,hrader styles in templayt fokder, interface

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
-const gulp = require('gulp');
-const minify = require('gulp-minify-css');
-const rename  =  require('gulp-rename');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
-gulp.task('hello', function(done) {
-  console.log('Привет мир!');
-  done();
-  });
+const sass = require('gulp-sass')
+
 
   // Static server
-gulp.task('browser-sync', function() {
-  browserSync.init({
+  function bs() {
+    serveSass();
+    browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
-gulp.task('minify', function() {
-  return gulp.src('css/*.css')
-  .pipe(minify())
-  .pipe(rename({
-    suffix: '.min'
-  }))
-  .pipe(gulp.dest('./minify.css'));
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
+
+function serveSass() {
+  return src("./sass/*.sass")
+  .pipe (sass())
+  .pipe(dest("./css"))
+  .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
