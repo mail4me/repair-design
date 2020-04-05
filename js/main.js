5fc1e48af8afc9fa2ffd4f0f03fdfcfe432fc846
Юлия Линовицкая <mail4me2401@yandex.ru>
2020-04-05 23:34:07 +0300

    feat: add function to modal close

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
    const modal = document.querySelector('.modal');
    const modalbtn = document.querySelectorAll('[data-toggle=modal]');
-   const closeBtn = document.querySelector('.modal__close');
+   const closeBtn = document.querySelector('.modal__close, .modal');
    const switchModal = () => {
      modal.classList.toggle('modal--visible');
     }
    modalbtn.forEach(element => {
      element.addEventListener('click', switchModal);
   }); 
-
+   
    closeBtn.addEventListener('click', switchModal);
 });
