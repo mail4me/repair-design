ba8a7c5c211877c4ff44e038b4677c66e0e308e3
Юлия Линовицкая <mail4me2401@yandex.ru>
2020-04-07 22:44:39 +0300

    feat: add js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+/*
 document.addEventListener("DOMContentLoaded", function(event) { 
    const modal = document.querySelector('.modal');
    const modalbtn = document.querySelectorAll('[data-toggle=modal]');
-   const closeBtn = document.querySelector('.modal__close, .modal');
+   const closeBtn = document.querySelector('.modal__close');
    const switchModal = () => {
      modal.classList.toggle('modal--visible');
     }
@@ -11,3 +12,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
    
    closeBtn.addEventListener('click', switchModal);
 });
+*/
+$(document).ready(function(){
+   const modal = $('.modal'),
+     modalBtn = $('[data-toggle=modal]'),
+     closeBtn = $('.modal__close');
+   modalBtn.on('click',function(){
+     modal.toggleClass('modal--visible') 
+   });
+   closeBtn.on('click', function(){
+      modal.toggleClass('modal--visible')
+   })
+})
